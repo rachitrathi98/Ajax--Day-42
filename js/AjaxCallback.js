fd1000d98d0ae512f5415ddccfdab559c31eca33
Rachit Rathi <rachitrathi98@gmail.com>
2020-11-26 21:08:13 +0530

    [Rachit] Add. View Employee using Callback

---
+++ js/AjaxCallback.js
@@ -0,0 +1,51 @@
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+function showTime() {
+	const date = new Date();
+   return date.getHours() + "Hrs:"  + date.getMinutes() + "Mins:" +date.getSeconds()+ "Secs";
+}
+
+function makeAJAXCall(methodType, url, callback, async = true, data = null) {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        // console.log(methodType+" State Changed Called at: "+showTime()+" RS: "+
+        //             xhr.readyState+" Status:"+xhr.status);  
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200 || xhr.status === 201){
+                callback(xhr.responseText);
+            } else if (xhr.status >= 400) {
+                console.log("Handle 400 Client Error or 500 Server Error at: "+showTime());
+            }
+        }
+    }
+    xhr.open(methodType, url, async);
+    if (data) {
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(data));
+     } else xhr.send();
+    console.log(methodType+" request sent to the server at: "+showTime());
+}
+
+const getURL = "http://127.0.0.1:3000/employees";
+function getUserDetails(data){
+    console.log("Get User Data at: " + showTime() + " data: " +data)
+ } 
+makeAJAXCall("GET", getURL, getUserDetails, true);
+console.log("Made GET AJAX Call to Server at "+showTime());
+
+const deleteURL = "http://localhost:3000/employees/9";
+function userDeleted(data){
+   console.log("User Deleted at: " + showTime() + " data: "+data)
+}
+makeAJAXCall("DELETE", deleteURL, userDeleted, false);
+console.log("Made DELETE AJAX Call to Server at "+showTime());
+
+
+const postURL = "http://localhost:3000/employees/";
+const emplData = {"name": "Shyam","salary": "3000"};
+function userAdded(data){
+   console.log("User Added at: " + showTime() + " data: "+data)
+}
+makeAJAXCall("POST", postURL, userAdded, true, emplData);
+
+console.log("Made POST AJAX Call to Server at "+showTime());
